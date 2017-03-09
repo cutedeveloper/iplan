d42401982f6cc93cab7cc0cb744874e705badd3e
saeed <sa.dadkhah@gmail.com>
2017-03-09 14:02:37 +0330

    GoalManager Class added

--- src/Goal.h
+++ src/Goal.h
@@ -2,15 +2,21 @@
 #define GOAL_H_
 
 #include <string>
+#include <memory>
+#include <vector>
 
 typedef size_t AcTick;
 
 class Goal
 {
+public:
+	typedef std::unique_ptr<Goal> GoalUniquePointer;
+	typedef std::vector<GoalUniquePointer> GoalChildren;
 private:
     std::string description;
     AcTick total_actick;
     AcTick done_actick;
+	GoalChildren children;
 };
 
 #endif
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+// In the name of God
+
+int main(int argc, char** argv)
+{
+
+}
